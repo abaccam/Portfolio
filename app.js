007ee4c87eb775fd50a868181bce64f6d11c7fde
abaccam <abaccam.simplon@gmail.com>
2018-02-15 17:06:21 +0100

    contact

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var positionElementInPage = $('#headerFlex').offset().top;
 $( window ).resize(function() {
 positionElementInPage = $('#headerFlex').offset().top;
 });
+
 $(window).scroll(
 function() {
 if ($(window).scrollTop() > positionElementInPage) {
@@ -19,4 +20,6 @@ $('#headerFlex').removeClass("fixedTop");
 $(window).scroll(function(){
 $("#bandDiv").css("opacity", 1 - $(window).scrollTop() / 750);
 });
+
+
 
