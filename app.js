c5a6e3d065e9682727d25d9489f0ea9e679a400b
abaccam <abaccam.simplon@gmail.com>
2018-04-12 12:18:52 +0200

    button 1

--- app.js
+++ app.js
@@ -21,5 +21,28 @@ $(window).scroll(function(){
 $("#bandDiv").css("opacity", 1 - $(window).scrollTop() / 750);
 });
 
+// Collapse Navbar
+let navbarCollapse = function() {
+  if ($("#mainNav").offset().top > 100) {
+    $("#mainNav").addClass("navbar-shrink");
+  } else {
+    $("#mainNav").removeClass("navbar-shrink");
+  }
+};
+// Collapse now if page is not at top
+navbarCollapse();
+// Collapse the navbar when page is scrolled
+$(window).scroll(navbarCollapse);
 
+// Modal popup$(function () {
+$('.portfolio-item').magnificPopup({
+  type: 'inline',
+  preloader: false,
+  focus: '#username',
+  modal: true
+});
+$(document).on('click', '.portfolio-modal-dismiss', function(e) {
+  e.preventDefault();
+  $.magnificPopup.close();
+});
 
